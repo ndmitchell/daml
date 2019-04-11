8502347e7a92eecb4ffff6c27117babd97694f20
Jonas Chevalier <42969603+jonas-chevalier-da@users.noreply.github.com>
2019-04-11 18:20:03 +0200

    install git and bash

--- infra/vsts_agent_windows_startup.ps1
+++ infra/vsts_agent_windows_startup.ps1
@@ -6,18 +6,18 @@
 # Install chocolatey
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+# Add-LocalGroupMember -Group "Administrators" -Member "vsts"
+
+## Install runtime dependencies
+
+choco install --yes git.install
+
 # Install scoop
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 
 # Create the VSTS user
 New-LocalUser "vsts" -NoPassword -FullName "VSTS agent"
 
-# Add-LocalGroupMember -Group "Administrators" -Member "vsts"
-
-
-# Install the VSTS agent
+## Install the VSTS agent
 choco install azure-pipelines-agent --yes --params "'/Token:${vsts_token} /Pool:${vsts_pool} /Url:https://${vsts_account}.visualstudio.com'"
 # TODO: /LogonAccount:.\vsts'"
-
-
-# TODO: make sure that the agent started
