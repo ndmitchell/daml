9116217c175ee250f0577fef6e4b99867d8aad9b
Tim Williams <tim.williams@digitalasset.com>
2019-04-15 15:37:17 +0200

    api-server-damlonx: make ReferenceServer command-line-configurable.
    This includes:
    - port
    - list of dars to load
    - enable bad read service

--- ledger/api-server-damlonx/reference/src/main/scala/com/daml/ledger/api/server/damlonx/reference/ReferenceServer.scala
+++ ledger/api-server-damlonx/reference/src/main/scala/com/daml/ledger/api/server/damlonx/reference/ReferenceServer.scala
@@ -7,11 +7,17 @@ import java.io.File
 import java.time.Instant
 import java.util.zip.ZipFile
 
+import akka.NotUsed
 import akka.actor.ActorSystem
+import akka.stream.scaladsl.Source
 import akka.stream.{ActorMaterializer, ActorMaterializerSettings, Supervision}
 import com.daml.ledger.api.server.damlonx.Server
 import com.daml.ledger.participant.state.index.v1.impl.reference.ReferenceIndexService
+import com.daml.ledger.participant.state.v1.impl.reference.Ledger
+import com.daml.ledger.participant.state.v1.{ReadService, Offset, Update}
 import com.digitalasset.daml.lf.archive.DarReader
+import com.digitalasset.daml.lf.data.ImmArray
+import com.digitalasset.daml.lf.transaction.GenTransaction
 import com.digitalasset.daml_lf.DamlLf.Archive
 import com.digitalasset.platform.server.services.testing.TimeServiceBackend
 import com.digitalasset.platform.services.time.TimeModel
@@ -30,10 +36,34 @@ object ReferenceServer extends App {
         Supervision.Stop
       })
 
+  val defaultConfig = Config(
+    port = 6865,
+    files = Seq(),
+    badServer = false
+  )
+
+  val argParser = new scopt.OptionParser[Config]("ledger-reference-server") {
+    head("Ledger Reference Server")
+    opt[Int]('p', "port")
+      .action((x, c) => c.copy(port = x))
+      .text("Ledger API server port. Defaults to 6865.")
+    opt[Seq[File]]('d', "dars")
+      .valueName("<dar1>,<dar2>...")
+      .action((x, c) => c.copy(files = x))
+      .text("DARs/dalfs to include")
+    opt[Unit]("bad-server")
+      .action((_, c) => c.copy(badServer = true))
+      .text("Simulate a badly behaving server that returns empty transactions. Defaults to false.")
+    help("help").text("Print this usage text")
+  }
+
+  val config = argParser
+    .parse(args, defaultConfig)
+    .getOrElse(sys.exit(1))
+
   val timeModel = TimeModel.reasonableDefault
   val tsb = TimeServiceBackend.simple(Instant.EPOCH)
-  val ledger =
-    new com.daml.ledger.participant.state.v1.impl.reference.Ledger(timeModel, tsb)
+  val ledger = new Ledger(timeModel, tsb)
 
   def archivesFromDar(file: File): List[Archive] = {
     DarReader[Archive](x => Try(Archive.parseFrom(x)))
@@ -43,21 +73,22 @@ object ReferenceServer extends App {
 
   // Parse DAR archives given as command-line arguments and upload them
   // to the ledger using a side-channel.
-  args.foreach { arg =>
-    archivesFromDar(new File(arg)).foreach { archive =>
+  config.files.foreach { f =>
+    archivesFromDar(f).foreach { archive =>
       logger.info(s"Uploading archive ${archive.getHash}...")
       ledger.uploadArchive(archive)
     }
   }
 
-  val indexService = ReferenceIndexService(ledger)
+  val indexService = ReferenceIndexService(
+    if (config.badServer) BadReadService(ledger) else ledger)
 
   // Block until the index service has been initialized, e.g. it has processed the
   // state initialization updates.
   indexService.waitUntilInitialized
 
   val server = Server(
-    serverPort = 6865,
+    serverPort = config.port,
     indexService = indexService,
     writeService = ledger,
     tsb
@@ -65,4 +96,21 @@ object ReferenceServer extends App {
 
   // Add a hook to close the server. Invoked when Ctrl-C is pressed.
   Runtime.getRuntime.addShutdownHook(new Thread(() => server.close()))
+}
+
+final case class Config(port: Int, files: Seq[File], badServer: Boolean)
+
+// simulate a bad read service by returning only
+// empty transactions.
+final case class BadReadService(ledger: Ledger) extends ReadService {
+  override def stateUpdates(beginAfter: Option[Offset]): Source[(Offset, Update), NotUsed] =
+    ledger.stateUpdates(beginAfter).map {
+      case (updateId, update) =>
+        val updatePrime = update match {
+          case tx: Update.TransactionAccepted =>
+            tx.copy(transaction = GenTransaction(Map(), ImmArray.empty))
+          case _ => update
+        }
+        (updateId, updatePrime)
+    }
 }
